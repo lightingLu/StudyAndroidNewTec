ec132e3ae9a3a08bfef566f63130b5e469d406eb
threelu <215154863@qq.com>
2018-05-08 21:52:19 +0800

    更新项目

--- MVVM/src/main/java/com/example/myapplication/MainActivity.kt
+++ MVVM/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.myapplication
 
-import android.support.v7.app.AppCompatActivity
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var ss:SwordMan
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val bind: ActivityMainBinding = DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main);
+        ss= SwordMan("test","哈哈")
+        bind.swordman=ss
+
     }
 }
---
+++ MVVM/src/main/java/com/example/myapplication/SwordMan.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication
+
+/**
+ * Created by lightlu on 2017/10/23.
+ */
+class SwordMan(name:String,level: String) {
+    var name:String = name
+    val level:String = level
+}
